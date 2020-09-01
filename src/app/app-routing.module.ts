70df2d26ebb0833459c70102ac003a301d76ad47
Eduardo Correia <eduardoxcorreia@outlook.com>
2020-09-01 23:15:47 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ContactspageComponent } from './contactspage/contactspage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'aboutpage', component: AboutpageComponent },
+  { path: 'contactspage', component: ContactspageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
