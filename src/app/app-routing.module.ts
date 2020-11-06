7fabde5a02dc390e38694b218580303b33ad7efc
Eduardo Correia <eduardoxcorreia@outlook.com>
2020-11-06 18:16:34 +0000

    updated website layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutpageComponent } from './aboutpage/aboutpage.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ContactspageComponent } from './contactspage/contactspage.component';
+import { AboutwebsiteComponent } from './aboutwebsite/aboutwebsite.component';
+import { PortfoliopageComponent } from './portfoliopage/portfoliopage.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'aboutpage', component: AboutpageComponent },
   { path: 'contactspage', component: ContactspageComponent },
+  { path: 'aboutwebsite', component: AboutwebsiteComponent },
+  { path: 'portfolio', component: PortfoliopageComponent },
 ];
 
 @NgModule({
---
+++ src/app/portfoliopage/portfoliopage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfoliopage',
+  templateUrl: './portfoliopage.component.html',
+  styleUrls: ['./portfoliopage.component.scss']
+})
+export class PortfoliopageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
